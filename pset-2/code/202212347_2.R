ce0528468c03ceae8ff902e035f8ec0d28626fd7
brayanflorez <90009617+brayanflorez@users.noreply.github.com>
2024-03-19 18:46:57 -0500

    commit 1

---
+++ pset-2/code/202212347_2.R
@@ -0,0 +1,23 @@
+########################################
+##                                    ##
+##  TALLER DE R. ECON-1302            ##
+##       PROBLEM SET 2                ##
+##                                    ##
+##  David Florez-202212347            ##
+## Maria Contreras-202014039          ##
+##                                    ##
+## R version 4.3.1 (2023-06-16 ucrt)  ##
+##                                    ##
+########################################
+
+##set up
+rm(list=ls())
+
+##Instalar y llamar librerías
+require(pacman)
+
+p_load(tidyverse, rio, skimr, janitor, data.table) 
+
+
+#############################################
+####INTRODUCCION A R: OPERADORES Y FUNCIONES
