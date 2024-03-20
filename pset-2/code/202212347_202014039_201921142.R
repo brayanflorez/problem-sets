01e5013c8a1c45c025e4dc087cf8191138bda910
mafecontreras <157652644+mafecontreras@users.noreply.github.com>
2024-03-20 18:42:40 -0500

    punto 5

--- pset-2/code/202212347_202014039_201921142.R
+++ pset-2/code/202212347_202014039_201921142.R
@@ -4,9 +4,8 @@
 ##       PROBLEM SET 2                ##
 ##                                    ##
 ##  David Florez-202212347            ##
-## Maria Contreras-202014039 
-##
-##                                    ##
+## Maria Contreras-202014039          ##
+##Lina Ramos-201921142                ##
 ## R version 4.3.1 (2023-06-16 ucrt)  ##
 ##                                    ##
 ########################################
@@ -17,7 +16,7 @@ rm(list=ls())
 ##Instalar y llamar librerías
 require(pacman)
 
-p_load(tidyverse, rio, skimr, janitor, haven, data.table) 
+p_load(tidyverse, rio, skimr, janitor, haven) 
 
 #############################################
 ####1. IMPORTAR/EXPORTAR BASES DE DATOS
@@ -47,8 +46,6 @@ identification <- mutate(identification,
                                                   p241>=35 &p241<50 ~ "Adulto",
                                                   p241>=50  ~ "Senior" ))
 
-#Joven (menores de 18), Adulto joven (18-35 años), Adulto (35-50 años), Senior (mayores de 50 años)
-
 #2.3
 location <- mutate(location, 
                    ambulante=ifelse(p3053==3 |p3053==4 | p3053==5,yes=1, no=0))
@@ -70,7 +67,7 @@ location_sub <- location[, c("directorio", "secuencia_p", "secuencia_encuesta", 
 #4.1
 combined_data <- data.frame(identification_sub, location_sub)
 
-####
+#Punto 5
 
 #5.1
 
@@ -80,8 +77,21 @@ table(combined_data$grupo_etario)
 table(combined_data$cod_depto)
 hist(combined_data$ambulante)
 
+#5.2
+combined_data%>% group_by(cod_depto,grupo_etario)%>%summarise(prop_ambu=mean(ambulante,na.rm=T))%>%
+  print(n = Inf)
 
+combined_data%>% group_by(cod_depto,grupo_etario)%>%summarise(mean_local=mean(p3054,na.rm=T))%>%
+  print(n = Inf)
 
-
-
-
+##Planteamos nuestro analisis entre numero de locales y la proporcion de negocios ambulantes por departamento
+##y edad. Encontramos que en el departemento del Choco los adultos jovenes (18-35) presentaron un mayor indice
+##de poseer un mayor numero locales, en comparativa con el resto de departamentos. Si bien el adulto (35-50) del Choco tambien
+##tiene una media significativa, es similar a la de otros departamentos como Huila. Ahora bien, la media en el
+##Choco dentro de la poblacion de adultos jovenes tuvo una proporcion del 43% respecto a la cantidad de puestos ambulantes.
+##Los datos sugieren entonces que la ventaja   compartiva de puestos del Choco no es una dato correlacionado con la formalidad del trabajo.
+##Un ejemplo de esto es Bogota, siendo la ciudad con mayor porcentaje de puestos ambulantes dentro de los adultos jovenes con un 61% y con
+## una cifra baja de numero de locales.Estas cifras  sugieren entonces que no hay un vinculo directo entre los departamentos que mas locales 
+##registran, tanto formales como informales, y aquellos que reportan mayores porcentajes de negocios ambulantes.Creemos que intuitivamente deberia
+##presentarse una correlacion pero la existencia de diferentes regulaciones,politicas regionales y diferencias de la densidad poblacional arrojan 
+##dichos resultados.
